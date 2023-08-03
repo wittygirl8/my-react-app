6e84dfd839ab8433f1429d194bd1ad709568c1d4
Riddhi Kumari Singh <riddhisi.it@gmail.com>
2023-08-03 20:56:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey, Riddhi Dhananjay Kumar Singh
         </a>
       </header>
     </div>
